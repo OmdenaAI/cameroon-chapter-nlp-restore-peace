59998a757ef899d99e35e5e3045c79edc5eee042
Jaime <jroccalayza@gmail.com>
2022-11-15 11:58:55 -0500

    fixing heads and function name

--- src/tasks/task-0-data-collection/from_search_engines/scraping_news_articles.py
+++ src/tasks/task-0-data-collection/from_search_engines/scraping_news_articles.py
@@ -30,7 +30,7 @@ def get_query(base_words_query, samples=10):
     return list_valid_arrays
 
 
-def get_news_links(list_queries, driver, n_pages=5):
+def get_news_links_google(list_queries, driver, n_pages=5):
     list_head_news = [] 
     enable_to_scrap = True
     for query in list_queries:
@@ -40,9 +40,14 @@ def get_news_links(list_queries, driver, n_pages=5):
             url = "http://www.google.com/search?q=" + query + "&start=" + str((page - 1) * 10)
             #delay = int(np.median(np.random.random(10)*10))
             delay = int(np.quantile(np.random.random(30)*30, np.random.choice([0.25, 0.5, 0.75], 1)))
+
             driver.get(url)
             soup = BeautifulSoup(driver.page_source, 'html.parser')
-            # soup = BeautifulSoup(r.text, 'html.parser')
+            
+            '''                                                                                                                                                       
+            r = requests.get(url, headers=headers)
+            soup = BeautifulSoup(r.text, 'html.parser')
+            '''                                              
         
             print('page:', page, 'delay:',delay)
             
@@ -114,7 +119,7 @@ def get_news_links_from_bing(list_queries, headers, n_pages=5):
 
     return list_head_news
 
-def scraping_text(df, link_column='link'):
+def scraping_text(df, headers, link_column='link'):
     
     list_news = []
     
@@ -125,7 +130,7 @@ def scraping_text(df, link_column='link'):
         time.sleep(delay)
 
         try:
-            html_tmp = requests.get(dict_data[link_column]).text        
+            html_tmp = requests.get(dict_data[link_column], headers=headers).text        
             not_error = True
         except Exception as e:
             not_error = False
